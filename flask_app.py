d67ad672fcd03326381ca34ce4cf6de347a285ac
Amit Kumar <officialamit558@gmail.com>
2024-03-28 10:23:13 +0530

    2nd commit

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, request
 import numpy as np
 import pickle
 import pandas as pd
 import flasgger
 from flasgger import Swagger
-
+import sklearn
 app=Flask(__name__)
 Swagger(app)
 
